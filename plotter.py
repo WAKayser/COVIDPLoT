d67d6ef0fd175568e46358bfe1f82803a77de83d
Wouter Kayser <wouterkayser@gmail.com>
2021-11-11 18:02:24 +0100

    Increase ylim and covid prediction set to correct date.

--- plotter.py
+++ plotter.py
@@ -131,7 +131,7 @@ def plot_save(data, light=True):
     per_covid = format(total_covid / 15_200_000 * 100, '.1f')
 
     covid_prediction = infection_predictor(covid,
-                                           same['date'].iloc[-1],
+                                           covid['date'].iloc[-1],
                                            data,
                                            shift=3)
 
@@ -199,7 +199,7 @@ def plot_save(data, light=True):
 
     # plt.axvline(covid['date'].iloc[-1], linewidth=3)
 
-    ax.set_ylim(0, 14000)
+    ax.set_ylim(0, 18000)
     ax.set_ylabel('Cases per day')
     ax2.set_ylabel('IC occupation per day', color='r')
     ax2.set_ylim(0, 1000)
